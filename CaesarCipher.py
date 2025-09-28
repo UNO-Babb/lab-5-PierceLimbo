5d6b9acfcd91aa277166f237e272cf30cada23db
Pierce Limbo <plimbo@unomaha.edu>
2025-09-28 18:27:56 -0500

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,3 +1,4 @@
+# Pierce Limbo ChaesarCipher 9/28/2025
 #Caesar Cipher
 #The Caesar cipher moves each letter forward in the alphabet by
 #the key.  The resulting message has all the letters advanced by 'key'
@@ -10,25 +11,37 @@ def encode(message, key):
     secret = ""
 
     for letter in message:
-        if (alpha.find(letter) >= 0): #check to see if the letter is actually a letter
+        if (alpha.find(letter) >= 0): 
             spot = (alpha.find(letter) + key) % 26
             secret = secret + alpha[spot]
-        else: # letter must have been a number, symbol, or punctuation.
+        else: 
             secret = secret + letter
 
     return secret
 
-#def decode(message, key):
-    #We will want to decode the message here.
+def decode(message, key):
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    message = message.upper()
+    plain = ""
 
+    for letter in message:
+        if (alpha.find(letter) >= 0):  
+            spot = (alpha.find(letter) - key) % 26  
+            plain = plain + alpha[spot]
+        else:  
+            plain = plain + letter
+
+    return plain
+    
 def main():
     message = input("Enter a message: ")
     key = int(input("Enter a key: "))
 
     secret = encode(message, key)
     print ("Encrypted:", secret)
-    #plaintext = decode(secret, key)
-    #print ("Decrypted:", plaintext)
+
+if __name__ == '__main__':
+  main()
 
 
 if __name__ == '__main__':
